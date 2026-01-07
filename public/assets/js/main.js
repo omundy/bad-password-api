5a9389ab2e2e4538c570f53a930320f3c084373e
Owen Mundy <omundy@gmail.com>
2026-01-07 16:34:09 -0500

    Add baseurl like big feelings and format

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,22 @@
 // main.js - client-side code to connect to the bad password API
 
+//////////////////////////////////////
+/////////////// INIT /////////////////
+//////////////////////////////////////
+
+// base url for 10.3
+let baseurl = "https://bad-password-api.vercel.app";
+
+// ⚠️ base url for localhost testing
+// baseurl = "http://localhost:3000";
+// ⚠️ 
+
+// 👉 update base url to pull from your own database (Chapter 9 wiki) ...
+baseurl = "";
+// 👈
+
+
+
 // REFERENCES
 let password = document.querySelector("#password");
 let button = document.querySelector("#submit");
@@ -7,35 +24,34 @@ let button = document.querySelector("#submit");
 // EVENTS
 window.addEventListener("load", updatePassword);
 document.addEventListener("submit", (e) => {
-  e.preventDefault();
-  updatePassword();
+    e.preventDefault();
+    updatePassword();
 });
 
 // add a change event listener to each option
 document.querySelectorAll("input[type=radio]").forEach((ele) => {
-  ele.addEventListener("change", updatePassword);
+    ele.addEventListener("change", updatePassword);
 });
 
 // called from load and user events
 async function updatePassword() {
-  // 👉 add code inside this function (from Chapter 9) ...
+    // 👉 add code inside this function (from Chapter 9) ...
 
-  // get the value from both groups
-  let group1 = document.querySelector("input[name=group1]:checked");
-  let group2 = document.querySelector("input[name=group2]:checked");
+    // get the value from both groups
+    let group1 = document.querySelector("input[name=group1]:checked");
+    let group2 = document.querySelector("input[name=group2]:checked");
 
-  // base url for API
-  // let url = "https://bad-password-api.glitch.me/api";
-  let url = "/api";
-  // append options to the end
-  url += "/custom?params=" + group1.value + "," + group2.value;
-  // console.log(url);
+    // append options to the end of api
+    let url = baseurl + "/api/custom?params=" + group1.value + "," + group2.value;
+    // console.log(url);
 
-  // request data
-  await fetch(url)
-    .then((response) => response.json())
-    .then((json) => {
-      console.log("fetch() response", json);
-      password.value = json.message;
-    });
+    // send fetch request
+    await fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log("fetch() response", json);
+            password.value = json.message;
+        });
+
+    // 👈
 }
